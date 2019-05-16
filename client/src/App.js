124337ef8ac506b5b62f53a62cc03295f9d1ab48
Nour Essoussi <nourcherifsoussi@gmail.com>
2019-05-16 11:38:36 -0400

    Boilerplate Ready To Use

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React, { Fragment, Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <Fragment>
+        <h1>Test</h1>
+      </Fragment>
+    );
+  }
+}
+
+export default App;
